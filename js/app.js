61fe9f821ba2945f1eba6e38a36cfbb36860c34e
Sara Masek <smasek09@gmail.com>
2021-06-09 11:03:53 -0400

    Continued work on search filter in app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 baguetteBox.run('.gallery');
 
-const search = new Filter('search', 'data-caption');
+// const search = new Filter('search', 'data-caption');
+
+
+function searchGallery() {
+    let input = document.getElementById("search").value;
+    let inputLowerCase = input.toLowerCase();
+    let photos = document.getElementsByClassName("photos");
+    let caption = document.getElementsByTagName("a").getAttribute("data-caption");
+
+    for (i = 0; i < photos.length; i++) {
+
+        if (!photos[i].innerHTML.toLowerCase().includes(inputLowerCase)) {
+            caption.style.display = "none";
+        }
+        else {
+            caption.style.display = "list-item";
+        }
+    }
+
+}
